c37785181e1ed5e9e5ee4a22afa8b0a6b9e25c88
Pablo99JP <pablo999jp@gmail.com>
2025-12-11 15:05:22 +0100

    estilos para movil con refactorizacion

---
+++ NeuralPlay/Views/remove-duplicates.ps1
@@ -0,0 +1,12 @@
+﻿$files = @(
+    "Equipo\Edit.cshtml",
+    "MiembroEquipo\Edit.cshtml"
+)
+
+foreach ($file in $files) {
+    $path = "c:\Users\pablo\Desktop\universidad\DSM\DSM-NeuralPlay\NeuralPlay\Views\$file"
+    $content = Get-Content $path -Raw
+    $content = $content -replace '    \.form-group \{ margin-bottom: \d+px; \}[\r\n]+', ''
+    $content | Set-Content $path -NoNewline
+}
+Write-Host "Archivos procesados" -ForegroundColor Green
